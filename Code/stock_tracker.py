47f223fb1b08e802406a068bc9e2aa71d55d8e38
mattyrobin8 <mattyrobin8@gmail.com>
2021-09-28 19:46:40 -0400

    Update stock_tracker.py
    Create Monday and Friday dataframes which will be combined later

--- Code/stock_tracker.py
+++ Code/stock_tracker.py
@@ -17,7 +17,7 @@ def stock_data(stock, begin_date, end_date):
     '''Retrieve opening and closing prices from Yahoo Finance'''
     df = yf.download(tickers = stock, period='1d', start = begin_date, end = end_date)
     df.reset_index(inplace=True)
-    return df[['Date', 'Open', 'Close']]
+    return df[['Date', 'Open']], df[['Date', 'Close']]
 
 def make_calendar(begin_date, end_date):
     '''Create calendar for Monday open and Friday close prices'''
@@ -25,8 +25,9 @@ def make_calendar(begin_date, end_date):
     df.reset_index(inplace=True)
     df.rename(columns = {'index':'Date'}, inplace = True)
     df['Day'] = df['Date'].dt.day_name()
-    df = df[(df['Day'] == 'Monday') | (df['Day'] == 'Friday')]
-    return df[['Date', 'Day']]
+    monday_df = df[(df['Day'] == 'Monday')]
+    friday_df = df[(df['Day'] == 'Friday')]
+    return monday_df[['Date', 'Day']], friday_df[['Date', 'Day']]
 
 def merge_data(df1, df2):
     '''Merge stock and calendar data'''
@@ -55,16 +56,20 @@ nyse = mcal.get_calendar('NYSE')
 def main():
 
     #Import stock
-    stock_df = stock_data(tickerSymbol, date.today() - timedelta(days), date.today())
-    print(stock_df)
+    opening_stock_df, closing_stock_df = stock_data(tickerSymbol, date.today() - timedelta(days), date.today())
+    print(opening_stock_df)
+    print(closing_stock_df)
 
     #Import calendar
-    calendar_df = make_calendar(date.today() - timedelta(days), date.today())
-    print(calendar_df)
+    monday_df, friday_df = make_calendar(date.today() - timedelta(days), date.today())
+    print(monday_df)
+    print(friday_df)
 
     #Merge stock and calendar
-    stock_calendar_df = merge_data(stock_df, calendar_df)
-    print(stock_calendar_df)
+    opening_calendar_df = merge_data(opening_stock_df, monday_df)
+    closing_calendar_df = merge_data(closing_stock_df, friday_df)
+    print(opening_calendar_df)
+    print(closing_calendar_df)
 
 #Run Main script and record runtime
 if __name__ == '__main__':
