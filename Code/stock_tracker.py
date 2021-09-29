875cac1a18fedd2b3317d4eb12c276189e54d554
mattyrobin8 <mattyrobin8@gmail.com>
2021-09-29 16:58:05 -0400

    Update stock_tracker.py
    Format the equal sign

--- Code/stock_tracker.py
+++ Code/stock_tracker.py
@@ -13,17 +13,17 @@ import time
 ###Define Functions###
 ######################
 
-def stock_data(stock, begin_date, end_date):
+def stock_data(stock, start_date, end_date):
     '''Retrieve opening and closing prices from Yahoo Finance'''
-    df = yf.download(tickers = stock, period='1d', start = begin_date, end = end_date)
+    df = yf.download(tickers=stock, period='1d', start=start_date, end=end_date)
     df.reset_index(inplace=True)
     return df[['Date', 'Open']], df[['Date', 'Close']]
 
-def make_calendar(begin_date, end_date):
+def make_calendar(start, end):
     '''Create calendar for Monday open and Friday close prices'''
-    df = nyse.schedule(start_date = begin_date, end_date = end_date)
+    df = nyse.schedule(start_date=start, end_date=end)
     df.reset_index(inplace=True)
-    df.rename(columns = {'index':'Date'}, inplace = True)
+    df.rename(columns={'index':'Date'}, inplace=True)
     df['Day'] = df['Date'].dt.day_name()
     monday_df = df[(df['Day'] == 'Monday')]
     friday_df = df[(df['Day'] == 'Friday')]
